a97f83353b07fa5ae5cc3e31a888a385b3f4c87d
Glenn Vandeuren <vandeurenglenn@gmail.com>
2019-05-06 22:42:40 +0200

    :ring: animate on win

--- src/slots/slot-ring-slot.js
+++ src/slots/slot-ring-slot.js
@@ -14,16 +14,50 @@ export default define(class SlotRingSlot extends HTMLElement {
   set icon(value) {
     this.shadowRoot.querySelector('slot-icon').icon = value;
   }
-  spin() {
+  win() {
+    this.line.style.background = '#fff';
+    this.line.style.height = Number(document.body.style.getPropertyValue('--slot-ring-slot-size').replace('px', '')) / 4 + 'px';
+    this.line.style.width = '2px';
+    this.line.style.display = 'block';
+    this.line.style.zIndex = '100';
+    this.line.style.position = 'absolute';
+    this.line.style.top = '50%';
+    this.line.style.left = '50%';
+    this.line.style.transform = 'translate(-50%, -50%)';
     return new Promise((resolve, reject) => {
-      animation.onend(() => {
+      this._animation.onfinish = () => {
         resolve();
-      })
-      animation.play();
+      }
+      this._lineAnimation.play();
+      this._animation.play();
     });
   }
   set index(value) {
     this.setAttribute('index', value);
+  }
+  connectedCallback() {
+    if (super.connectedCallback) super.connectedCallback();
+    this.line = document.createElement('span');
+    this.shadowRoot.appendChild(this.line)
+    this._lineAnimation = this.line.animate(
+      [
+        { transform: 'none', filter: 'blur(0)' },
+        {
+          transform: `translateY(-50%) scaleY(2)`,
+          filter: 'blur(0)',
+        },
+        {
+          transform: `translateY(-50%) scaleY(4)`,
+          filter: 'blur(0)',
+        },
+      ],
+      {
+        duration: 250,
+        easing: 'ease-in-out',
+        fill: 'forwards'
+      },
+    );
+    this._lineAnimation.cancel();
   }
   constructor() {
     super();
@@ -45,21 +79,25 @@ export default define(class SlotRingSlot extends HTMLElement {
     <slot-icon></slot-icon>
     `;
 
-    animation = this.animate(
+    this._animation = this.animate(
       [
         { transform: 'none', filter: 'blur(0)' },
         { filter: 'blur(2px)', offset: 0.5 },
         {
-          transform: `translateY(100%)`,
+          transform: `scale(1.2)`,
+          filter: 'blur(10px)',
+        },
+        {
+          transform: `scale(1)`,
           filter: 'blur(0)',
         },
       ],
       {
-        duration: 1000,
+        duration: 500,
         easing: 'ease-in-out',
         fill: 'forwards'
       },
     );
-    animation.cancel();
+    this._animation.cancel();
   }
 })
